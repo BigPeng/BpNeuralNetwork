1d5fc601672de4870760c6221fdc9c148a117a62
pengji <jiqunpeng@gmail.com>
2014-06-25 16:12:01 +0800

    ignore no data file

--- data/rmlable.py
+++
@@ -1,4 +0,0 @@
-import sys
-for line in sys.stdin:
-	attrs,lable = line.strip().rsplit(',',1)
-	print attrs
--- data/shuffle.py
+++
@@ -1,9 +0,0 @@
-import sys
-import random
-data = []
-for line in sys.stdin:
-	data.append(line.strip())
-random.shuffle(data)
-for line in data:
-	print line
-	
